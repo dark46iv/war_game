26f7dd315811fe7f090090366b54317f3dbc7d38
dark46 <dark.46.iv@yandex.ru>
2019-02-05 16:11:39 +0300

    сделал ограничение кол-ва пуль

--- alien_invasion.py
+++ alien_invasion.py
@@ -33,7 +33,6 @@ def run_game():
         for bullet in bullets.copy():
             if bullet.rect.bottom <= 0:
                 bullets.remove(bullet)
-        print(len(bullets))
 
         # При каждом проходе цикла перерисовывается экран.
         gf.update_screen(ai_settings, screen, ship, bullets)
